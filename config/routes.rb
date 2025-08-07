e6a37cc9b66102485780dc4c8c0741fa231dfde1
Matthieu Minier <matthieu.minier75@gmail.com>
2025-08-07 16:51:43 +1000

    add cors gem to fix 404 from axios

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/create'
-  get 'tasks/update'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,5 +7,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :tasks, only: [:index, :create, :update]
+  resources :tasks
 end
